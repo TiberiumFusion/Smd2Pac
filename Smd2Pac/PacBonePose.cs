2c81583d7423bdf4cf11ac1a762469ddefd8094f
TiberiumFusion <6332277+TiberiumFusion@users.noreply.github.com>
2021-12-14 01:16:02 -0500

    Comment clarity

--- Smd2Pac/PacBonePose.cs
+++ Smd2Pac/PacBonePose.cs
@@ -13,11 +13,13 @@ namespace TiberiumFusion.Smd2Pac
         public float MR = 0f; // +Y translation (right/north)
         public float MU = 0f; // +Z translation (up)
 
-        // Source's "forward"/"right" terminology is misleading. Models face +Y/north/right while their right side faces +X/east/forward.
+        // Source's "forward"/"right" terminology is misleading.
+        // - At design time, models face +Y while their right side faces +X.
+        // - Ingame, models face +X while their right side faces -Y.
 
         public float RF = 0f; // pitch
         public float RR = 0f; // yaw
         public float RU = 0f; // roll
-            // Yes these are out of order because either Source and/or PAC3 is weird
+            // Yes these are out of order because either Source is weird
     }
 }
